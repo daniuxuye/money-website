4cb2a74de883ec32fefd24eae59f3913ce23a3bd
daniuxuye <1071940917@qq.com>
2020-04-11 18:37:05 +0800

    deploy01

---
+++ precache-manifest.03cc95701aced3cf685c3fee698c706a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbfbbd5c6510b3c69d56",
+    "url": "/money-website/css/app.103cfc92.css"
+  },
+  {
+    "revision": "114f50f056b5d6a4af088e5cdc5487be",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "dbfbbd5c6510b3c69d56",
+    "url": "/money-website/js/app.74cf6012.js"
+  },
+  {
+    "revision": "7716c195ccd91595b01d",
+    "url": "/money-website/js/chunk-vendors.4e887dfd.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "aff081094b3cb462f41b1f2a4f1beced",
+    "url": "/money-website/photo.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
